1e9f2c0bf1b11d650f27c9d25d5bf34a5c75fd87
asmmbd <129310936+asmmbd@users.noreply.github.com>
2023-03-31 17:55:52 +0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -130,3 +130,27 @@ if ("serviceWorker" in navigator) {
       console.log(error);
     });
 }
+
+let deferredPrompt;
+const addBtn = document.querySelector(".add-button");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  
+  e.preventDefault();
+  deferredPrompt = e;
+
+  
+
+  addBtn.addEventListener("click", () => {
+    
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
