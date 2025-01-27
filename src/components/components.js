1d308892ff4a69ac82e2c680761006ab5bff8bc1
asmmbd <129310936+asmmbd@users.noreply.github.com>
2025-01-28 02:01:48 +0600

    Update components.js

--- src/components/components.js
+++ src/components/components.js
@@ -6,7 +6,8 @@ function banglaNumber(number) {
     .map(digit => (/\d/.test(digit) ? bengaliDigits[digit] : digit))
     .join('');
 }
-function banglaMonth(date){
+
+function banglaMonth(date) {
   const banglaMonths = [
         "জানুয়ারি", "ফেব্রুয়ারি", "মার্চ", "এপ্রিল", "মে", "জুন",
         "জুলাই", "আগস্ট", "সেপ্টেম্বর", "অক্টোবর", "নভেম্বর", "ডিসেম্বর"
@@ -14,6 +15,11 @@ function banglaMonth(date){
   const currentMonthIndex = new Date(date).getMonth();
   return banglaMonths[currentMonthIndex];
 };
+
+function dateFormater(d){
+  let date = new Date(d);
+  return `${banglaNumber(date.getDate())} ${banglaMonth(date.getMonth())}, ${banglaNumber(date.getFullYear())}`
+}
 
 
 class Notice extends HTMLElement {
@@ -58,4 +64,47 @@ class Notice extends HTMLElement {
   }
 }
 
+class Holiday extends HTMLElement {
+  constructor() {
+    super();
+    this.notices = [];
+  }
+  render() {
+    this.innerHTML = `
+    <div class='notice-board'>
+    <h5 class="mb-2 py-2 text-center text-white prime-color rounded-top">নোটিশ বোর্ড</h5>
+    <div class='px-2'>
+      ${this.notices.map(notice => (`<div class='border-bottom d-flex align-items-center gap-2 p-2 mb-2'>
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="height: 40px; width:40px;" color="#000000" fill="none">
+            <path d="M17 2V5M7 2V5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M13 3.5H11C7.22876 3.5 5.34315 3.5 4.17157 4.67157C3 5.84315 3 7.72876 3 11.5V14C3 17.7712 3 19.6569 4.17157 20.8284C5.34315 22 7.22876 22 11 22H13C16.7712 22 18.6569 22 19.8284 20.8284C21 19.6569 21 17.7712 21 14V11.5C21 7.72876 21 5.84315 19.8284 4.67157C18.6569 3.5 16.7712 3.5 13 3.5Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M3.5 8.5H20.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M9 15.5C9 15.5 10.5 16 11 17.5C11 17.5 13.1765 13.5 16 12.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </div>
+        <div class='media-body'>
+          <span class="" style="font-size: 18px;">${dateFormater(notice.createdAt)} - ${dateFormater(notice.createdAt)}</span><br />
+          <a href='#' target='_blank' class="notice-text text-dark ">${notice.title}</a>
+        </div>
+      </div>`)).join(' ')}
+    </div>
+    <h6 class="mt-2 my-0 py-2 text-center rounded-bottom" style="background-color: #1ab33d"><a href="/notice" class="text-white "> আরো দেখুন<i class='bi bi-chevron-right'></i></a></h6>
+  </div>
+    `;
+  }
+  connectedCallback() {
+    this.render()
+    fetch('https://678ff67e49875e5a1a93fa27.mockapi.io/api/v1/notices')
+      .then(res => res.json())
+      .then(data => {
+        this.notices = data;
+        this.render()
+      })
+      .catch(err => console.error('Failed to fetch videos:', err));
+  }
+}
+
+
 customElements.define('notice-board', Notice);
+customElements.define('holiday-board', Holiday);
